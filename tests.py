90e616c7b590e1bbb2ed1259384b6fe9b831a062
Ivan Ermakov <ermakov93id@gmail.com>
2024-06-20 12:27:40 +0400

    adding info in readme.md, adding parameterization шin test.py and creating 2 tesfrom test_add_new_book_invalid_lengt adding info in readme.md, adding parameterization шin test.py and creating 2 tesfrom test_add_new_book_invalid_lengt

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -8,26 +10,44 @@ class TestBooksCollector:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    #def test_add_new_book_add_two_books(self):
         # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+        #collector = BooksCollector()
 
         # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        #collector.add_new_book('Гордость и предубеждение и зомби')
+        #collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        #assert len(collector.get_books_rating()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
 
-    def test_add_new_book_invalid_length(self):
+    def test_add_new_book_add_books(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert collector.books_genre['Гордость и предубеждение и зомби'] == ''
+
+    def test_add_new_book_length_more_40(self):
+        collector = BooksCollector()
+        collector.add_new_book('x' * 41)
+        assert "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" not in collector.books_genre
+    def test_add_new_book_length_is_0(self):
         collector = BooksCollector()
         collector.add_new_book('')
-        collector.add_new_book('x' * 41)
-        assert len(collector.get_books_genre()) == 0
+        assert "" not in collector.books_genre
+
+    @pytest.mark.parametrize('books, genre', [('Шерлок Холмс', 'Детективы'),
+                                              ('Гарри Поттер', 'Фатнастика')])
+    def test_get_list_of_favorites_books_have_books(self, books, genre):
+        collector = BooksCollector()
+        collector.add_new_book(books)
+        collector.set_book_genre(books, genre)
+        collector.add_book_in_favorites(books)
+        assert books in collector.get_list_of_favorites_books()
+
 
     def test_set_book_genre(self):
         collector = BooksCollector()
@@ -93,9 +113,8 @@ class TestBooksCollector:
 
     def test_books_with_no_genre(self):
         collector = BooksCollector()
-        collector.add_new_book('Гарри Поттер')
-        collector.add_new_book('Властелин колец')
-        assert collector.get_books_with_specific_genre('') == ['Гарри Поттер', 'Властелин колец']
+        collector.add_new_book('Шерлок Холмс')
+        assert collector.get_books_with_specific_genre('') == []
 
     def test_invalid_genre(self):
         collector = BooksCollector()
